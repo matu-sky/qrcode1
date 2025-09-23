aa23e4f4366b55d5fb9a49947c4efd8889fb063e
Your Name <you@example.com>
2025-09-23 07:17:37 +0000

    fix: 배포 오류 수정 (미사용 import 제거)

--- src/pages/DisplayPage.tsx
+++ src/pages/DisplayPage.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
-import { Button, ButtonGroup } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import './DisplayPage.css'; // Import the CSS
 
 export default function DisplayPage() {
